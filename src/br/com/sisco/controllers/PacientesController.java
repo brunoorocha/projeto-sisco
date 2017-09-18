0b1ffaf52b4de620e6ccf9c2d7a11b5e55ad6687
Bruno Rocha <mail.brunorocha@gmai.com>
2017-09-18 14:29:51 -0300

    Adição de pasta nbproject ao arquivo .gitignore

--- src/br/com/sisco/controllers/PacientesController.java
+++ src/br/com/sisco/controllers/PacientesController.java
@@ -10,10 +10,12 @@ import br.com.sisco.models.Paciente;
 import br.com.sisco.views.PacientesListCell;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -29,6 +31,18 @@ public class PacientesController implements Initializable {
     @FXML private ListView pacientesListView;
     @FXML private StackPane rootPane;
     @FXML private Label agendaTab;
+    
+    @FXML private Button editarButton;
+    @FXML private Button salvarButton;
+                
+    
+    // Campos da área de Dados Pessoais             
+    @FXML private TextField textFieldNomeCompleto;
+    @FXML private TextField textFieldMatricula;
+    @FXML private TextField textFieldTelefone;
+    @FXML private ChoiceBox choiceBoxVinculo;
+    @FXML private DatePicker datePickerDataNascimento;
+    
 
     private ObservableList<Paciente> pacientesList;
 
@@ -58,5 +72,41 @@ public class PacientesController implements Initializable {
         
         this.pacientesList.clear();        
         this.pacientesList.setAll(new PacienteDAO().buscarPaciente(nome));
+    }
+    
+    
+    /*
+    
+        Action do botão de editar
+    
+    */   
+    
+    @FXML
+    private void editarButtonAction() {
+        salvarButton.setVisible(true);
+        
+        this.setDisableFields(false);
+    }
+    
+    
+    /*
+    
+        Action do botão de salvar
+    
+    */   
+    
+    @FXML
+    private void salvarButtonAction() {
+        salvarButton.setVisible(false);
+        
+        this.setDisableFields(true);
+    }            
+    
+    private void setDisableFields(boolean value) {
+        textFieldNomeCompleto.setDisable(value);
+        textFieldMatricula.setDisable(value);
+        textFieldTelefone.setDisable(value);
+        choiceBoxVinculo.setDisable(value);
+        datePickerDataNascimento.setDisable(value);
     }
 }
